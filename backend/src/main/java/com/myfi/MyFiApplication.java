ea2ab3ef04719d1e016ba3a377718d6adae1333c
Mukul Singh <mukulsingh1248@gmail.com>
2025-04-02 03:14:50 +0530

    Initial commit: Setup Spring Boot backend with SQLite and React frontend

---
+++ backend/src/main/java/com/myfi/MyFiApplication.java
@@ -0,0 +1,11 @@
+package com.myfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyFiApplication.class, args);
+    }
+} 
